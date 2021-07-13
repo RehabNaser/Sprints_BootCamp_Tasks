a720d831f4b879e9ac3caf330f44963e2093ac5b
RehabNaser <rehabnaser.stf@gmail.com>
2021-07-13 08:04:04 +0200

    Edit MC_REGISTERS File on Drivers Folder

--- ES_L1_B/Drivers/MCAL/ATMEGA32/MC_REGISTERS.h
+++ ES_L1_B/Drivers/MCAL/ATMEGA32/MC_REGISTERS.h
@@ -8,7 +8,7 @@
 #ifndef __MC_REGISTERS__
 #define __MC_REGISTERS__
 
-#define F_CPU 10000000ul
+#define F_CPU 16000000ul
 
 //FILES INCLUDING
 #include "STD_TYPES.h"
@@ -27,9 +27,9 @@
 /*                         GENERAL REGISTERS                            */
 /************************************************************************/
 //GENERAL REGISTERS
-#define SFIOR *((volatile ptr_int8_t)(0X50))
-#define SREG *((volatile ptr_int8_t)(0X5F))
-#define MCUCSR *((volatile ptr_int8_t)(0X54))
+#define SFIOR *((volatile ptr_uint8_t)(0X50))
+#define SREG *((volatile ptr_uint8_t)(0X5F))
+#define MCUCSR *((volatile ptr_uint8_t)(0X54))
 
 
 //SFIOR REGISTER BITS
@@ -50,36 +50,36 @@
 /************************************************************************/
 //PORTA REGISTERS
 #define PORTA_BASE 0X39
-#define PINA *((volatile ptr_int8_t)(PORTA_BASE))
-#define DDRA *((volatile ptr_int8_t)(PORTA_BASE+1))
-#define PORTA *((volatile ptr_int8_t)(PORTA_BASE+2))
+#define PINA *((volatile ptr_uint8_t)(PORTA_BASE))
+#define DDRA *((volatile ptr_uint8_t)(PORTA_BASE+1))
+#define PORTA *((volatile ptr_uint8_t)(PORTA_BASE+2))
 
 //PORTB REGISTERS 
 #define PORTB_BASE 0X36
-#define PINB *((volatile ptr_int8_t)(PORTB_BASE))
-#define DDRB *((volatile ptr_int8_t)(PORTB_BASE+1))
-#define PORTB *((volatile ptr_int8_t)(PORTB_BASE+2))
+#define PINB *((volatile ptr_uint8_t)(PORTB_BASE))
+#define DDRB *((volatile ptr_uint8_t)(PORTB_BASE+1))
+#define PORTB *((volatile ptr_uint8_t)(PORTB_BASE+2))
 
 //PORTC REGISTERS
 #define PORTC_BASE 0X33
-#define PINC *((volatile ptr_int8_t)(PORTC_BASE))
-#define DDRC *((volatile ptr_int8_t)(PORTC_BASE+1))
-#define PORTC *((volatile ptr_int8_t)(PORTC_BASE+2))
+#define PINC *((volatile ptr_uint8_t)(PORTC_BASE))
+#define DDRC *((volatile ptr_uint8_t)(PORTC_BASE+1))
+#define PORTC *((volatile ptr_uint8_t)(PORTC_BASE+2))
 
 //PORTD REGISTERS
 #define PORTD_BASE 0X30
-#define PIND *((volatile ptr_int8_t)(PORTD_BASE))
-#define DDRD *((volatile ptr_int8_t)(PORTD_BASE+1))
-#define PORTD *((volatile ptr_int8_t)(PORTD_BASE+2))
+#define PIND *((volatile ptr_uint8_t)(PORTD_BASE))
+#define DDRD *((volatile ptr_uint8_t)(PORTD_BASE+1))
+#define PORTD *((volatile ptr_uint8_t)(PORTD_BASE+2))
 /************************************************************************/
 /*                          ADC REGISTERS                               */
 /************************************************************************/
 //ADC REGISTERS
-#define ADCL *((volatile ptr_int8_t)(0X24))
-#define ADCH *((volatile ptr_int8_t)(0X25))
-#define ADC *((volatile ptr_int16_t)(0X24))
-#define ADCSRA *((volatile ptr_int8_t)(0X26))
-#define ADMUX *((volatile ptr_int8_t)(0X27))
+#define ADCL *((volatile ptr_uint8_t)(0X24))
+#define ADCH *((volatile ptr_uint8_t)(0X25))
+#define ADC *((volatile ptr_uint16_t)(0X24))
+#define ADCSRA *((volatile ptr_uint8_t)(0X26))
+#define ADMUX *((volatile ptr_uint8_t)(0X27))
 
 
 //ADMUX REGISTER BITS
@@ -104,10 +104,10 @@
 /*                        EXTERNAL INTERRUPT REGISTERS                  */
 /************************************************************************/
 //EXTERNAL INTERRUPT REGISTERS
-#define GICR *((volatile ptr_int8_t)(0X5B))
-#define GIFR *((volatile ptr_int8_t)(0X5A))
-#define MCUCR *((volatile ptr_int8_t)(0X55))
-#define MCUCSR *((volatile ptr_int8_t)(0X54))
+#define GICR *((volatile ptr_uint8_t)(0X5B))
+#define GIFR *((volatile ptr_uint8_t)(0X5A))
+#define MCUCR *((volatile ptr_uint8_t)(0X55))
+#define MCUCSR *((volatile ptr_uint8_t)(0X54))
 
 
 //GICR REGISTER BITS
@@ -129,7 +129,7 @@
 /*                      WATCHDOG TIMER REGISTERS                        */
 /************************************************************************/
 //WATCHDOG TIMER REGISTERS  
-#define WDTCR *((volatile ptr_int8_t)(0X41))
+#define WDTCR *((volatile ptr_uint8_t)(0X41))
 
 
 //WDTCR REGISTER BITS
@@ -142,8 +142,8 @@
 /*                         GENERAL TIMERS REGISTERS                     */
 /************************************************************************/
 //GENERAL TIMERS REGISTERS
-#define TIMSK *((volatile ptr_int8_t)(0X59))
-#define TIFR *((volatile ptr_int8_t)(0X58))
+#define TIMSK *((volatile ptr_uint8_t)(0X59))
+#define TIFR *((volatile ptr_uint8_t)(0X58))
 
 
 //TIMSKR REGISTER BITS
@@ -168,9 +168,9 @@
 /*                         TIMER0 REGISTERS                             */
 /************************************************************************/
 //TIMER0 REGISTERS 
-#define TCCR0 *((volatile ptr_int8_t)(0X53))
-#define TCNT0 *((volatile ptr_int8_t)(0X52))
-#define OCR0 *((volatile ptr_int8_t)(0X5C))
+#define TCCR0 *((volatile ptr_uint8_t)(0X53))
+#define TCNT0 *((volatile ptr_uint8_t)(0X52))
+#define OCR0 *((volatile ptr_uint8_t)(0X5C))
 
 
 //TCCR0 REGISTER BITS
@@ -186,20 +186,20 @@
 /*                         TIMER1 REGISTERS                             */
 /************************************************************************/
 //TIMER1 REGISTERS 
-#define TCCR1A *((volatile ptr_int8_t)(0X4F))
-#define TCCR1B *((volatile ptr_int8_t)(0X4E))
-#define TCNT1H *((volatile ptr_int8_t)(0X4D))
-#define TCNT1L *((volatile ptr_int8_t)(0X4C))
-#define TCNT1 *((volatile ptr_int16_t)(0X4C))
-#define OCR1AH *((volatile ptr_int8_t)(0X4B))
-#define OCR1AL *((volatile ptr_int8_t)(0X4A))
-#define OCR1A *((volatile ptr_int16_t)(0X4A))
-#define OCR1BH *((volatile ptr_int8_t)(0X49))
-#define OCR1BL *((volatile ptr_int8_t)(0X48))
-#define OCR1B *((volatile ptr_int16_t)(0X48))
-#define ICR1H *((volatile ptr_int8_t)(0X47))
-#define ICR1L *((volatile ptr_int8_t)(0X46))
-#define ICR1 *((volatile ptr_int16_t)(0X46))
+#define TCCR1A *((volatile ptr_uint8_t)(0X4F))
+#define TCCR1B *((volatile ptr_uint8_t)(0X4E))
+#define TCNT1H *((volatile ptr_uint8_t)(0X4D))
+#define TCNT1L *((volatile ptr_uint8_t)(0X4C))
+#define TCNT1 *((volatile ptr_uint16_t)(0X4C))
+#define OCR1AH *((volatile ptr_uint8_t)(0X4B))
+#define OCR1AL *((volatile ptr_uint8_t)(0X4A))
+#define OCR1A *((volatile ptr_uint16_t)(0X4A))
+#define OCR1BH *((volatile ptr_uint8_t)(0X49))
+#define OCR1BL *((volatile ptr_uint8_t)(0X48))
+#define OCR1B *((volatile ptr_uint16_t)(0X48))
+#define ICR1H *((volatile ptr_uint8_t)(0X47))
+#define ICR1L *((volatile ptr_uint8_t)(0X46))
+#define ICR1 *((volatile ptr_uint16_t)(0X46))
 
 //TCCR1A REGISTER BITS
 #define WGM10 BIT0
@@ -222,9 +222,9 @@
 /*                         TIMER2 REGISTERS                             */
 /************************************************************************/
 //TIMER2 REGISTERS 
-#define TCCR2 *((volatile ptr_int8_t)(0X45))
-#define TCNT2 *((volatile ptr_int8_t)(0X44))
-#define OCR2 *((volatile ptr_int8_t)(0X43))
+#define TCCR2 *((volatile ptr_uint8_t)(0X45))
+#define TCNT2 *((volatile ptr_uint8_t)(0X44))
+#define OCR2 *((volatile ptr_uint8_t)(0X43))
 
 
 //TCCR2 REGISTER BITS
@@ -240,11 +240,11 @@
 /*                         EEPROM REGISTERS                             */
 /************************************************************************/
 //EEPROM REGISTERS
-#define EEARH *((volatile ptr_int8_t)(0X3F))
-#define EEARL *((volatile ptr_int8_t)(0X3E))
-#define EEAR *((volatile ptr_int16_t)(0X3E))
-#define EEDR *((volatile ptr_int8_t)(0X3D))
-#define EECR *((volatile ptr_int8_t)(0X3C))
+#define EEARH *((volatile ptr_uint8_t)(0X3F))
+#define EEARL *((volatile ptr_uint8_t)(0X3E))
+#define EEAR *((volatile ptr_uint16_t)(0X3E))
+#define EEDR *((volatile ptr_uint8_t)(0X3D))
+#define EECR *((volatile ptr_uint8_t)(0X3C))
 
 
 //EECR REGISTER BITS
@@ -256,12 +256,12 @@
 /*                          USART REGISTERS                             */
 /************************************************************************/
 //USART REGISTERS
-#define UDR *((volatile ptr_int8_t)(0X2C))
-#define UCSRA *((volatile ptr_int8_t)(0X2B))
-#define UCSRB *((volatile ptr_int8_t)(0X2A))
-#define UCSRC *((volatile ptr_int8_t)(0X40))
-#define UBRRL *((volatile ptr_int8_t)(0X29))
-#define UBRRH *((volatile ptr_int8_t)(0X40))
+#define UDR *((volatile ptr_uint8_t)(0X2C))
+#define UCSRA *((volatile ptr_uint8_t)(0X2B))
+#define UCSRB *((volatile ptr_uint8_t)(0X2A))
+#define UCSRC *((volatile ptr_uint8_t)(0X40))
+#define UBRRL *((volatile ptr_uint8_t)(0X29))
+#define UBRRH *((volatile ptr_uint8_t)(0X40))
 
 
 //UCSRA REGISTER BITS
@@ -295,9 +295,9 @@
 /*                           SPI REGISTERS                              */
 /************************************************************************/
 //SPI REGISTERS
-#define SPDR *((volatile ptr_int8_t)(0X2F))
-#define SPSR *((volatile ptr_int8_t)(0X2E))
-#define SPCR *((volatile ptr_int8_t)(0X2D))
+#define SPDR *((volatile ptr_uint8_t)(0X2F))
+#define SPSR *((volatile ptr_uint8_t)(0X2E))
+#define SPCR *((volatile ptr_uint8_t)(0X2D))
 
 
 //SPSR REGISTER BITS
@@ -317,7 +317,7 @@
 /*                           TWI REGISTERS                              */
 /************************************************************************/
 //TWI REGISTERS
-#define TWCR *((volatile ptr_int8_t)(0X56))
+#define TWCR *((volatile ptr_uint8_t)(0X56))
 
 
 //TWCR REGISTER BITS
@@ -326,7 +326,7 @@
 /*                   ANALOG COMPARATOR REGISTERS                        */
 /************************************************************************/
 //ANALOG COMPARATOR REGISTERS  
-#define ACSR *((volatile ptr_int8_t)(0X28))
+#define ACSR *((volatile ptr_uint8_t)(0X28))
 
 
 //ACSR REGISTER BITS
