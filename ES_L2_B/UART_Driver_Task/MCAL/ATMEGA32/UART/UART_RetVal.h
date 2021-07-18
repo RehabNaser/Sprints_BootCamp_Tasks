a3d3bf1a3bc82f12b2d585298916d8abd5cc85e3
RehabNaser <rehabnaser.stf@gmail.com>
2021-07-18 20:08:48 +0200

    Add UART_Driver_Task Folder on ES_L2_B Folder

---
+++ ES_L2_B/UART_Driver_Task/MCAL/ATMEGA32/UART/UART_RetVal.h
@@ -0,0 +1,16 @@
+/*
+ * File Name: UART_RetVal.h
+ * Creation Date: 09/07/2020
+ * Author: Rehab
+ * Description: Header File for UART Module Return Value for Error Handling....
+ */ 
+
+#ifndef __UART_RetVal__
+#define __UART_RetVal__
+
+#define ERROR_OK 0 //No Error
+
+#define ERROR_NOK 1
+
+
+#endif
