2fbc9d36236b38dde45f9992dd920a7728391a5e
RehabNaser <rehabnaser.stf@gmail.com>
2021-07-26 16:03:08 +0200

    Modify STD_TYPES.h File onDrivers Folder on Es_L1_B Folder

--- ES_L1_B/Drivers/LIB/STD_TYPES.h
+++ ES_L1_B/Drivers/LIB/STD_TYPES.h
@@ -8,28 +8,28 @@
 #ifndef __STD_TYPES__
 #define __STD_TYPES__
 
-typedef unsigned char 			uint8_t;
-typedef unsigned short int 		uint16_t;
-typedef unsigned long int 		uint32_t;
-typedef unsigned long long int 	uint64_t;
+volatile typedef unsigned char				uint8_t;
+volatile typedef unsigned short int			uint16_t;
+volatile typedef unsigned long int			uint32_t;
+volatile typedef unsigned long long int		uint64_t;
 
-typedef signed char 			int8_t;
-typedef signed short int 		int16_t;
-typedef signed long int 		int32_t;
-typedef signed long long int 	int64_t;
+volatile typedef signed char 			int8_t;
+volatile typedef signed short int 		int16_t;
+volatile typedef signed long int 		int32_t;
+volatile typedef signed long long int 	int64_t;
 
-typedef float 					float32_t;
-typedef double 					double64_t;
+volatile typedef float					float32_t;
+volatile typedef double					double64_t;
 
-typedef uint8_t* 				ptr_uint8_t;
-typedef uint16_t* 				ptr_uint16_t;
-typedef uint32_t* 				ptr_uint32_t;
-typedef uint64_t* 				ptr_uint64_t;
+volatile typedef uint8_t* 				ptr_uint8_t;
+volatile typedef uint16_t* 				ptr_uint16_t;
+volatile typedef uint32_t* 				ptr_uint32_t;
+volatile typedef uint64_t* 				ptr_uint64_t;
 
-typedef int8_t* 				ptr_int8_t;
-typedef int16_t* 				ptr_int16_t;
-typedef int32_t* 				ptr_int32_t;
-typedef int64_t* 				ptr_int64_t;
+volatile	typedef int8_t* 				ptr_int8_t;
+volatile	typedef int16_t* 				ptr_int16_t;
+volatile	typedef int32_t* 				ptr_int32_t;
+volatile	typedef int64_t* 				ptr_int64_t;
 
-typedef void(*Ptr_VoidFuncVoid_t)(void);
+volatile typedef void(*Ptr_VoidFuncVoid_t)(void);
 #endif
