42043f24d40b2d38def6edd3b58aa42f2559d7f2
RehabNaser <rehabnaser.stf@gmail.com>
2021-08-07 12:53:25 +0200

    Adding Chat_Task Folder on ES_L2_B Folder

---
+++ ES_L2_B/Chat_Task/Master/Drivers/APP/Master_main.c
@@ -0,0 +1,11 @@
+#include "Master_Chat.h" 
+
+int main()
+{
+	SystemInit();
+	
+	while(1)
+	{
+		SystemUpdate();
+	}
+}
---
+++ ES_L2_B/Chat_Task/Slave/Drivers/APP/Slave_main.c
@@ -0,0 +1,11 @@
+#include "Slave_Chat.h" 
+
+int main()
+{
+	SystemInit();
+	
+	while(1)
+	{
+		SystemUpdate();
+	}
+}
